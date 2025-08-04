43b7d39bd9d406f293234c80c4257a36fe141fec
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-04 15:47:15 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <sstream>
+#include <gtest/gtest.h>
 #include <vector>
-#include <cassert>
 #include <string>
 
 struct ColorPair {
@@ -16,48 +14,18 @@ std::vector<ColorPair> getColorPairs() {
     std::vector<ColorPair> colorPairs;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            // Bug here: minorColor[i] used instead of minorColor[j]
+            // Bug: wrong minorColor[i] instead of minorColor[j]
             colorPairs.push_back({i * 5 + j, majorColor[i], minorColor[i]});
         }
     }
     return colorPairs;
 }
 
-std::string formatColorPair(const ColorPair& pair) {
-    std::ostringstream oss;
-    oss << pair.pairNumber << " | " << pair.majorColor << " | " << pair.minorColor;
-    return oss.str();
-}
-
-int printColorMap() {
+TEST(ColorMapTest, ExposesMisalignedBug) {
     auto colorPairs = getColorPairs();
-    for (const auto& pair : colorPairs) {
-        std::cout << formatColorPair(pair) << "\n";
-    }
-    return colorPairs.size();
+    ASSERT_EQ(colorPairs.size(), 25);
+
+    EXPECT_EQ(colorPairs[0].minorColor, "Blue");   // Passes
+    EXPECT_EQ(colorPairs[1].minorColor, "Orange"); // FAILS with bug
+    EXPECT_EQ(colorPairs[5].minorColor, "Blue");   // FAILS with bug
 }
-
-// Strong tests that make the bug visible
-void testPrintColorMap() {
-    auto colorPairs = getColorPairs();
-    assert(colorPairs.size() == 25);
-
-    // Now test specific entries
-    assert(colorPairs[0].majorColor == "White");
-    assert(colorPairs[0].minorColor == "Blue"); // This will fail due to bug
-
-    assert(colorPairs[1].minorColor == "Orange"); // This will also fail
-    assert(colorPairs[5].majorColor == "Red");
-    assert(colorPairs[5].minorColor == "Blue"); // Should be Blue, will be Orange (bug)
-
-    std::cout << "All tests should have failed if bug is present!\n";
-}
-
-int main() {
-    testPrintColorMap();  // Correct function name
-    printColorMap();
-    return 0;
-}
-
-
-
