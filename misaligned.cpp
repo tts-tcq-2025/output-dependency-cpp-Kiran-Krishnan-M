28770b7252ed2e5c315982ec701441bbec5352e2
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-04 16:53:20 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -20,7 +20,7 @@ std::vector<ColorPair> getColorMap() {
             colorPairs.push_back({
                 i * 5 + j,
                 majorColors[i],
-                minorColors[i]  // Bug: should be minorColors[j]
+                minorColors[i]  // Bug is preserved here (should be minorColors[j])
             });
         }
     }
@@ -41,22 +41,19 @@ void testPrintColorMap() {
     assert(colorMap.size() == 25);
     
     // Test 2: Verify some specific color pairs
-    // This assertion will pass by chance due to the bug
     assert(colorMap[0].number == 0);
     assert(colorMap[0].majorColor == "White");
-    assert(colorMap[0].minorColor == "Blue");
+    assert(colorMap[0].minorColor == "Blue");  // This will fail due to the bug
     
-    // This assertion will fail because the minor color is incorrect
     assert(colorMap[5].number == 5);
     assert(colorMap[5].majorColor == "Red");
-    assert(colorMap[5].minorColor == "Orange"); // Expected: "Orange", Actual: "Blue"
+    assert(colorMap[5].minorColor == "Orange");  // This will fail due to the bug
     
     // Test 3: Verify all minor colors are correct
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
             int index = i * 5 + j;
-            // This assertion will fail as the bug uses minorColors[i] instead of minorColors[j]
-            assert(colorMap[index].minorColor == minorColors[j]);
+            assert(colorMap[index].minorColor == minorColors[j]);  // This will fail
         }
     }
     
@@ -66,9 +63,4 @@ void testPrintColorMap() {
     }
     
     std::cout << "All tests passed!\n";
-}
-
-int main() {
-    testPrintColorMap();
-    return 0;
 }
