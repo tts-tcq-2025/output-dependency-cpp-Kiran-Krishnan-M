dd90fd5e4dd621251a279b7fa6bd518bafb75744
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-07 18:48:24 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -30,10 +30,33 @@ void testPrintColorMap() {
     std::vector<std::string> colorMap = getColorMap();
     assert(colorMap.size() == 25);
 
-    // Stronger test: check specific entries
-    assert(colorMap[0] == "0 | White | Blue");  // Should fail due to bug
-    assert(colorMap[6] == "6 | Red | Green");   // Should fail due to bug
-    assert(colorMap[24] == "24 | Violet | Slate"); // Should fail due to bug
+
+    assert(colorMap[0] == "0 | White | Blue");  
+    assert(colorMap[1] == "1 | White | Orange");
+    assert(colorMap[2] == "2 | White | Green");
+    assert(colorMap[3] == "3 | White | Brown");
+    assert(colorMap[4] == "4 | White | Slate");
+    assert(colorMap[5] == "5 | Red | Blue");
+    assert(colorMap[6] == "6 | Red | Orange"); 
+    assert(colorMap[7] == "7 | Red | Green");  
+    assert(colorMap[8] == "8 | Red | Brown");
+    assert(colorMap[9] == "9 | Red | Slate");
+    assert(colorMap[10] == "10 | Black | Blue");
+    assert(colorMap[11] == "11 | Black | Orange"); 
+    assert(colorMap[12] == "12 | Black | Green");  
+    assert(colorMap[13] == "13 | Black | Brown");
+    assert(colorMap[14] == "14 | Black | Slate");
+    assert(colorMap[15] == "15 | Yellow | Blue");
+    assert(colorMap[16] == "16 | Yellow | Orange");  
+    assert(colorMap[17] == "17 | Yellow | Green");  
+    assert(colorMap[18] == "18 | Yellow | Brown");
+    assert(colorMap[19] == "19 | Yellow | Slate");
+    assert(colorMap[20] == "20 | Violet | Blue");
+    assert(colorMap[21] == "21 | Violet | Orange");  
+    assert(colorMap[22] == "22 | Violet | Green");  
+    assert(colorMap[23] == "23 | Violet | Brown");
+    assert(colorMap[24] == "24 | Violet | Slate"); 
 
     std::cout << "All is well (maybe!)\n";
 }
+
