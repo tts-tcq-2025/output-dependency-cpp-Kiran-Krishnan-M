aac58963b29f27fc66fd1100166138c5b37da567
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-18 13:58:15 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -8,7 +8,7 @@ namespace WeatherSpace
 {    
     class IWeatherSensor {
         public:
-            virtual double TemperatureInC() const = 0;
+            virtual double TemperatureInC() const = 0; 
             virtual int Precipitation() const = 0;
             virtual int Humidity() const = 0;
             virtual int WindSpeedKMPH() const = 0;
